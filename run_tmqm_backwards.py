50ed43ed940e76edbecc6737b02a6f6b5e4d8bed
chrisiacovella <chris.iacovella@gmail.com>
2025-01-08 23:41:02 -0800

    small changes to make it easier to eventually restart

--- run_tmqm_backwards.py
+++ run_tmqm_backwards.py
@@ -3,7 +3,7 @@ from time import time
 import h5py
 import json
 from sqlitedict import SqliteDict
-
+from loguru import logger
 
 filepath = "/home/cri/datasets/hdf5_files/tmqm_dataset_v0.hdf5"
 from utils import OpenWithLock
@@ -11,8 +11,9 @@ from utils import OpenWithLock
 data_input = None
 with OpenWithLock(f"status.lockfile", "w") as lock_file:
     with SqliteDict("tmqm.db", tablename="status", autocommit=True) as status_db:
-        for key in status_db.keys():
-
+        keys = list(status_db.keys())
+        for i in range(len(keys) - 1, 0, -1):
+            key = keys[i]
             if status_db[key] == "not_submitted":
                 with h5py.File(filepath, "r") as f:
                     data_input = load_config(f, key)
@@ -20,16 +21,16 @@ with OpenWithLock(f"status.lockfile", "w") as lock_file:
                 status_db[key] = "submitted"
                 break
 
-print("starting: ", data_input.name)
-print("n_atoms: ", data_input.geometry.shape[1])
+logger.debug(f"starting: {data_input.name}")
+logger.debug(f"n_atoms:  {data_input.geometry.shape[1]}")
 
 start = time()
 xtb_properties = run_xtb_calc(data_input)
 end = time()
 
-print("name: ", data_input.name)
-print("n_atoms: ", xtb_properties.geometry.shape[1])
-print(f"Time taken: {end-start}")
+logger.debug(f"name: {data_input.name}")
+logger.debug(f"n_atoms:  {data_input.geometry.shape[1]}")
+logger.info(f"Time taken: {end-start}")
 
 with SqliteDict("tmqm.db", tablename="results", autocommit=True) as results_db:
     results_db[data_input.name] = xtb_properties
